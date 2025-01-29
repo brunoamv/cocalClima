be7d59bb226a23a91654e343d223822ec7edf77f
brunoamv <bruno.amv@gmail.com>
2025-01-29 17:40:40 -0300

    vai

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -30,7 +30,7 @@ def create_payment(request):
         },
         "back_urls": {
             "success": "http://13.58.251.9:8000/payment-success/", ## PROD
-            "failure": "http://127.0.0.1:8000/",
+            "failure": "http://13.58.251.9:8000/",
         },
         "auto_return": "approved"
     }
