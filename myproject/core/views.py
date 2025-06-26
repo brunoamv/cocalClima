420a7eaae727c5a7ea583ff5d81519f2578301c1
brunoamv <bruno.amv@gmail.com>
2025-06-26 00:00:12 -0300

    Atualiza YOUTUBE_VIDEO_ID automaticamente

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -15,7 +15,7 @@ MERCADO_PAGO_ACCESS_TOKEN = "APP_USR-6572778228467438-012815-7995636b4be0f51ec60
 
 
 YOUTUBE_API_KEY = "AIzaSyAfNYAuhX5za5hQpZk3Dx5cesgGULWuVIE"
-YOUTUBE_VIDEO_ID = "lTpM-Ce7qZo"
+YOUTUBE_VIDEO_ID = "iAezoSpwCg0"
 
 def home(request):
     return render(request, "index.html")
